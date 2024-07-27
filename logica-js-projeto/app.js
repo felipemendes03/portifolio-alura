d9c59a0ba03c8fe71bc0534708ab57e17911a516
Felipe Mendes De Castro <137222215+felipemendes03@users.noreply.github.com>
2024-07-26 21:09:35 -0300

    Add files via upload

---
+++ logica-js-projeto/app.js
@@ -0,0 +1,24 @@
+alert('Boas Vindas ao jogo do múmero secreto');
+let numeroMaximo = 50;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto ) {
+   chute = prompt (`Escolha  um número entre 1 e ${numeroMaximo} `);
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+             alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+if (tentativas > 1)
+ alert(`isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
